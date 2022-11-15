76489b1a5b14be7c3d116668aa7dad6a4b90b1ee
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2022-11-15 16:33:22 +0500

    Add input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Body from './companents/Body/Body';
 import Header from './companents/Header/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <Body />
     </>
   );
 }
---
+++ src/companents/Body/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Body.css";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="body__cart"></div>
+    </div>
+  );
+};
+
+export default Body;
